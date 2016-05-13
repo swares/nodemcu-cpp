de35d987bbf7e4932f0aa508e0e6db05da5ead7a
swares <waresscott@gmail.com>
2016-05-13 11:35:08 -0600

    notes

--- output.ino
+++ output.ino
@@ -19,3 +19,26 @@ bool output_sdcard_debug_log = 0;
 # Use Arduino AJAX Web Server with pages saved on sdcard or eeprom or program mem.
 # - Javascript, AJAX, Images, Links, CSS.
 # http://blog.startingelectronics.com/arduino-web-server-tutorial/
+
+#  Space saving measures
+# use conditional compilation
+void setup(){
+#if defined(DEVMODE)
+    Serial.begin(115200);
+    Serial.print("Devmode ON");
+#else
+  // Setup the LCD
+    myGLCD.InitLCD();
+#endif
+
+# use strings in flash
+Serial.println(F("Sram sram sram sram. Lovely sram! Wonderful sram! Sram sra-a-a-a-a-am sram sra-a-a-a-a-am sram. Lovely sram! Lovely sram! Lovely sram! Lovely sram! Lovely sram! Sram sram sram sram!"));
+
+// save some unsigned ints
+const PROGMEM  uint16_t charSet[]  = { 65000, 32796, 16843, 10, 11234};
+
+// save some chars
+const char signMessage[] PROGMEM  = {"I AM PREDATOR,  UNSEEN COMBATANT. CREATED BY THE UNITED STATES DEPART"};
+
+
+# Trim down unneeded functions / code in libs
