7a293f710b5adc1448cabce6b7c5c9e8c0403019
swares <waresscott@gmail.com>
2016-05-12 22:48:25 -0600

    Lightning sensorr

--- config.ino
+++ config.ino
@@ -1,1 +1,24 @@
 # Code for Configuration Parameters
+
+// The AS3935 communicates via SPI or I2C. 
+// This example uses the I2C interface via the I2C lib, not Wire lib
+#include "I2C.h"
+// include Playing With Fusion AXS3935 libraries
+#include "PWFusion_AS3935_I2C.h"
+
+// interrupt trigger global var        
+volatile int8_t AS3935_ISR_Trig = 0;
+
+// defines for hardware config
+#define SI_PIN               9
+#define IRQ_PIN              2        // digital pins 2 and 3 are available for interrupt capability
+#define AS3935_ADD           0x03     // x03 - standard PWF SEN-39001-R01 config
+#define AS3935_CAPACITANCE   72       // <-- SET THIS VALUE TO THE NUMBER LISTED ON YOUR BOARD 
+
+// defines for general chip settings
+#define AS3935_INDOORS       0
+#define AS3935_OUTDOORS      1
+#define AS3935_DIST_DIS      0
+#define AS3935_DIST_EN       1
+
+// ------------------------------
