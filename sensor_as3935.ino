d7a1976e40d80cddf092a9c4604905e4e7400fea
swares <waresscott@gmail.com>
2016-05-12 22:50:14 -0600

    Upd

--- sensor_as3935.ino
+++ sensor_as3935.ino
@@ -78,21 +78,6 @@
 // include Playing With Fusion AXS3935 libraries
 #include "PWFusion_AS3935_I2C.h"
 
-// interrupt trigger global var        
-volatile int8_t AS3935_ISR_Trig = 0;
-
-// defines for hardware config
-#define SI_PIN               9
-#define IRQ_PIN              2        // digital pins 2 and 3 are available for interrupt capability
-#define AS3935_ADD           0x03     // x03 - standard PWF SEN-39001-R01 config
-#define AS3935_CAPACITANCE   72       // <-- SET THIS VALUE TO THE NUMBER LISTED ON YOUR BOARD 
-
-// defines for general chip settings
-#define AS3935_INDOORS       0
-#define AS3935_OUTDOORS      1
-#define AS3935_DIST_DIS      0
-#define AS3935_DIST_EN       1
-
 // prototypes
 void AS3935_ISR();
 
