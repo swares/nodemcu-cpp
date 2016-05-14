6a48e987fd75d1eaf050f9b3a943a5dcf5adf9a2
swares <waresscott@gmail.com>
2016-05-14 16:52:45 -0500

    Update config_output.ino

--- config_output.ino
+++ config_output.ino
@@ -5,6 +5,10 @@
 #define output_webserver;
 #define output_mqtt;
 
+//# Use Arduino AJAX Web Server with pages saved on sdcard or eeprom or program mem.
+//# - Javascript, AJAX, Images, Links, CSS.
+//# htDS3231_AT24C32log.startingelectronics.com/arduino-web-server-tutorial/
+
 // load web pages if output_webserver is defined
 #if defined(output_webserver)
   // http headers
@@ -35,44 +39,3 @@
   #endif
 #endif
 
-//# Use Arduino AJAX Web Server with pages saved on sdcard or eeprom or program mem.
-//# - Javascript, AJAX, Images, Links, CSS.
-//# htDS3231_AT24C32log.startingelectronics.com/arduino-web-server-tutorial/
-
-//# use strings in flash
-//Serial.println(F("Sram sram sram sram. Lovely sram! Wonderful sram! Sram sra-a-a-a-a-am sram sra-a-a-a-a-am sram. Lovely sram! Lovely sram! Lovely sram! Lovely sram! Lovely sram! Sram sram sram sram!"));
-
-// save some unsigned ints
-// const PROGMEM  uint16_t charSet[]  = { 65000, 32796, 16843, 10, 11234};
-
-//# Trim down unneeded functions / code in libs
-
-// Memory Lifetime
-// Flash   10k Write Cycles
-// EEPROM 100k Write Cycles
-// Sram / Dram Unlimited
-// +---------------------+----------------+-----------------------+------------------------+------------------------+
-// |                     | Arduino UNO R3 |     Arduino Nano      |         ESP-12         |        ESP-12E         |
-// +---------------------+----------------+-----------------------+------------------------+------------------------+
-// | Power               | 5V             | 5V                    | 3,3V                   | 3,3V                   |
-// | Io max              | 40mA           | 40mA                  | 12mA                   | 12mA                   |
-// | Clock Frequency     | 16MHz          | 16MHz                 | 26-52MHz               | 26-52MHz               |
-// | Microcontroller     | ATmega328      | ATmega168 / ATmega328 | ESP8266                | ESP8266                |
-// | Flash Memory        | 32KB           | 16 KB / 32KB          | 512KB                  | 512KB                  |
-// | SRAM                | 2 KB           | 1KB / 2 KB            | 64 KB SRAM / 96KB DRAM | 64 KB SRAM / 96KB DRAM |
-// | EEPROM              | 1 KB           | 512 bytes / 1 KB      | 4M                      | ?                      |
-// | USB                 | Yes            | Yes                   | No                     | No                     |
-// | External Power Port | Yes            | No                    | No                     | No                     |
-// | Reset Button        | Yes            | Yes                   | No                     | No                     |
-// | Pins                | 32             | 30                    | 16                     | 22                     |
-// | GPIO                | 14             | 14                    | 13 (9 Digital I/O)     | 16 (11 Digital I/O)    |
-// | Analog I/O          | 6              | 8                     | 1                      | 1                      |
-// | WiFi                | No             | No                    | Yes                    | Yes                    |
-// | Temp Sensor         | No             | No                    | Yes                    | Yes                    |
-// | UART                | 1              | 1                     | 1                      | 1                      |
-// | PWM                 | 6              | 6                     | 9                      | 9                      |
-// | FCC Approved        | Yes            | Yes                   | Yes                    | Yes                    |
-// | SPI / I2C           | Yes            | Yes                   | Yes                    | Yes                    |
-// | Price (Banggood)    | $ 4,10         | $ 3,20                | $ 3,80                 | $ 3,19                 |
-// +---------------------+----------------+-----------------------+------------------------+------------------------+
-// - See more at: http://www.esp8266.com/viewtopic.php?f=6&t=4348#sthash.59oBHYJ1.dpuf
