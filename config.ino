b6117a5296890a2399a3331d2a22b3b3f57d452c
swares <waresscott@gmail.com>
2016-05-14 13:16:15 -0500

    added ifdef loading of lightning sensor cfgs

--- config.ino
+++ config.ino
@@ -13,22 +13,25 @@
 //#define enable_ds3231_at24c32
 
 //# sensor parameters below
-// The AS3935 communicates via SPI or I2C. 
-// This example uses the I2C interface via the I2C lib, not Wire lib
 
-// interrupt trigger global var        
-volatile int8_t AS3935_ISR_Trig = 0;
+// if defined load params for the as3935 lightning sensor
+#if defined(enable_as3935)
+  // The AS3935 communicates via SPI or I2C. 
+  // This example uses the I2C interface via the I2C lib, not Wire lib
 
-// defines for hardware config
-#define SI_PIN               9
-#define IRQ_PIN              2        // digital pins 2 and 3 are available for interrupt capability
-#define AS3935_ADD           0x03     // x03 - standard PWF SEN-39001-R01 config
-#define AS3935_CAPACITANCE   72       // <-- SET THIS VALUE TO THE NUMBER LISTED ON YOUR BOARD 
+  // interrupt trigger global var        
+  volatile int8_t AS3935_ISR_Trig = 0;
 
-// defines for general chip settings
-#define AS3935_INDOORS       0
-#define AS3935_OUTDOORS      1
-#define AS3935_DIST_DIS      0
-#define AS3935_DIST_EN       1
+  // defines for hardware config
+  #define SI_PIN               9
+  #define IRQ_PIN              2        // digital pins 2 and 3 are available for interrupt capability
+  #define AS3935_ADD           0x03     // x03 - standard PWF SEN-39001-R01 config
+  #define AS3935_CAPACITANCE   72       // <-- SET THIS VALUE TO THE NUMBER LISTED ON YOUR BOARD 
 
+  // defines for general chip settings
+  #define AS3935_INDOORS       0
+  #define AS3935_OUTDOORS      1
+  #define AS3935_DIST_DIS      0
+  #define AS3935_DIST_EN       1
+#endif
 // ------------------------------
