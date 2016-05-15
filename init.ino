46027c2ddc87e0303f2baa0867a995ea0158a930
swares <waresscott@gmail.com>
2016-05-14 19:44:35 -0500

    Update init.ino

--- init.ino
+++ init.ino
@@ -3,11 +3,22 @@
 #include config.ino
 #include config_output.ino
 
-#ifdef
+#ifdef enable_rtc
   #include rtc.ino
 #endif
 
-#ifdef
-  #include webserver.ino
+#ifdef enable_sdcard
+  #include sdcard.ino
 #endif
 
+#ifdef enable_wifi
+  #include wifi.ino
+
+  #ifdef enable_ntp
+    #include rtc.ino
+  #endif
+
+  #ifdef enable_webserver
+    #include webserver.ino
+  #endif
+#endif
