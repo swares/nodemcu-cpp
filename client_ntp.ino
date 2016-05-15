fb0f01e2039042fb9056efc6317b1c1098a35887
swares <waresscott@gmail.com>
2016-05-15 17:14:13 -0500

    Update client_ntp.ino

--- client_ntp.ino
+++ client_ntp.ino
@@ -38,24 +38,21 @@ WiFiUDP udp;
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
 
   // We start by connecting to a WiFi network
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, pass);
+//  Serial.print("Connecting to ");
+//  Serial.println(ssid);
+//  WiFi.begin(ssid, pass);
   
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(500);
+//    Serial.print(".");
+//  }
+//  Serial.println("");
   
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+//  Serial.println("WiFi connected");
+//  Serial.println("IP address: ");
+//  Serial.println(WiFi.localIP());
 
   Serial.println("Starting UDP");
   udp.begin(localPort);
