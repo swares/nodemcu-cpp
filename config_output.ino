806bca8a4e571acaa84a7fb5278f817ab0650527
swares <waresscott@gmail.com>
2016-05-14 19:26:47 -0500

    oultine for mqtt messages

--- config_output.ino
+++ config_output.ino
@@ -100,5 +100,26 @@
 
 // load mqtt messages if output_mqtt is defined
 #if defined(output_mqtt)
+  // load mqtt messages
 
+  // load mqtt message if enable_switches is defined
+  #if defined(enable_switches)
+//    const char web_switches[]   PROGMEM  = { "<!DOCTYPE html><html><head><title>Environmental Monitoring Station - Switches</title><script>function GetSwitchState() {nocache = '&nocache=' + Math.random() * 1000000;var request = new XMLHttpRequest();request.onreadystatechange = function() {if (this.readyState == 4) {if (this.status == 200) {if (this.responseText != null) {document.getElementById('switch_txt'.innerHTML = this.responseText;}}}};request.open('GET', 'ajax_switch' + nocache, true);request.send(null);setTimeout('GetSwitchState()', 1000);}</script></head><body onload='GetSwitchState()'><h1>Environmental Monitoring Station - Switches</h1><p id='switch_txt'>Switch state: Not requested...</p></body></html>" };
+  #endif
+  // load mqtt message if enable_bmp180 is defined
+  #if defined(enable_bmp180)
+//    const char web_bmp180[]  PROGMEM  = { "<!DOCTYPE html><html><head><title>Environmental Monitoring Station - BMP180</title><script>function Get_BMP180_State() {nocache = '&nocache=' + Math.random() * 1000000;var request = new XMLHttpRequest();request.onreadystatechange = function() {if (this.readyState == 4) {if (this.status == 200) {if (this.responseText != null) {document.getElementById('bmp180_txt'.innerHTML = this.responseText;}}}};request.open('GET', 'ajax_switch' + nocache, true);request.send(null);setTimeout('Get_BMP180_State()', 1000);}</script></head><body onload='Get_BMP180_State()'><h1>Environmental Monitoring Station - BMP180</h1><p id='bmp180_txt'>State: Not requested...</p></body></html>" };
+  #endif
+  // load mqtt message if enable_dht22 is defined
+  #if defined(enable_dht22)
+//    const char web_dht22[]   PROGMEM  = { "<!DOCTYPE html><html><head><title>Environmental Monitoring Station - DHT22</title><script>function Get_DHT22_State() {nocache = '&nocache=' + Math.random() * 1000000;var request = new XMLHttpRequest();request.onreadystatechange = function() {if (this.readyState == 4) {if (this.status == 200) {if (this.responseText != null) {document.getElementById('dht22_txt'.innerHTML = this.responseText;}}}};request.open('GET', 'ajax_switch' + nocache, true);request.send(null);setTimeout('Get_DHT22_State()', 1000);}</script></head><body onload='Get_DHT22_State()'><h1>Environmental Monitoring Station - DHT22</h1><p id='dht22_txt'>State: Not requested...</p></body></html>" };
+  #endif
+  // load mqtt message if enable_as3935 is defined
+  #if defined(enable_as3935)
+//    const char web_as3935[]  PROGMEM  = { "<!DOCTYPE html><html><head><title>Environmental Monitoring Station - AS3935</title><script>function Get_AS3935_State() {nocache = '&nocache=' + Math.random() * 1000000;var request = new XMLHttpRequest();request.onreadystatechange = function() {if (this.readyState == 4) {if (this.status == 200) {if (this.responseText != null) {document.getElementById('as3935_txt'.innerHTML = this.responseText;}}}};request.open('GET', 'ajax_switch' + nocache, true);request.send(null);setTimeout('Get_AS3935_State()', 1000);}</script></head><body onload='Get_AS3935_State()'><h1>Environmental Monitoring Station - AS3935</h1><p id='as3935_txt'>State: Not requested...</p></body></html>" };
+  #endif
+  // load mqtt message if enable_ds3231_at24c32 is defined
+  #if defined(enable_ds3231_at24c32)
+//    const char web_ds3231_at24c32[] PROGMEM  = { "<!DOCTYPE html><html><head><title>Environmental Monitoring Station - DS3231_AT24C32</title><script>function Get_DS3231_AT24C32_State() {nocache = '&nocache=' + Math.random() * 1000000;var request = new XMLHttpRequest();request.onreadystatechange = function() {if (this.readyState == 4) {if (this.status == 200) {if (this.responseText != null) {document.getElementById('ds3231_at24c32_txt'.innerHTML = this.responseText;}}}};request.open('GET', 'ajax_switch' + nocache, true);request.send(null);setTimeout('Get_DS3231_AT24C32_State()', 1000);}</script></head><body onload='Get_DS3231_AT24C32_State()'><h1>Environmental Monitoring Station - DS3231_AT24C32</h1><p id='ds3231_at24c32_txt'>State: Not requested...</p></body></html>" };
+  #endif
 #endif
