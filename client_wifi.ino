068203a5463835571ce79421b1d39603d0b41270
swares <waresscott@gmail.com>
2016-05-15 23:08:03 -0500

    Update client_wifi.ino

--- client_wifi.ino
+++ client_wifi.ino
@@ -1,8 +1,7 @@
-# Place holder for WIFI networking code
+//# Place holder for WIFI networking code
 
-void setup() {
+private connect_to_ap() {
   // We start by connecting to a WiFi network
-
   #if defined(output_serial)  
     Serial.println();
     Serial.println();
@@ -22,69 +21,14 @@ void setup() {
     Serial.println("");
     Serial.println(serial_wifi_connected);  
     Serial.println(serial_wifi_ip_address);
-    Serial.println(WiFi.localIP());
+    Serial.println(WiF
+    i.localIP());
   #endif
+  return WiFi.localIP()
 }
 
-int value = 0;
-
-void loop() {
-  delay(5000);
-  ++value;
-
-  #if defined(output_serial)
-    Serial.print(serial_wifi_connecting);
-    Serial.println(host);
-  #endif
-  
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(host, httpPort)) {
-    #if defined(output_serial)
-      Serial.println(serial_wifi_connection_failed);
-    #endif
-    return;
-  }
-  
-  // We now create a URI for the request
-  String url = "/input/";
-  url += streamId;
-  url += "?private_key=";
-  url += privateKey;
-  url += "&value=";
-  url += value;
-  
-  #if defined(output_serial)
-    Serial.print(serial_wifi_rerquesting_url0);
-    Serial.println(url);
-  #endif
-  // This will send the request to the server
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      #if defined(output_serial)
-        Serial.println(serial_wifi_client_timeout);
-      #endif
-      client.stop();
-      return;
-    }
-  }
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    #if defined(output_serial)
-      Serial.print(line);
-    #endif
-  }
-  
-  #if defined(output_serial)
-    Serial.println();
-    Serial.println(serial_wifi_closing);
-  #endif
+void start(){
+  connect_to_ap()  
+//  myip = connect_to_ap()  
+//  return myip
 }
-
